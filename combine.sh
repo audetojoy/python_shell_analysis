00dd9df28b5a79754795a29693deba32821f4a0a
audetojoy <abouagnon@NSR-BVTQJL1.ucsf.edu>
2016-12-10 09:39:23 -0800

    First draft of combine.sh

---
+++ combine.sh
@@ -0,0 +1,20 @@
+
+#Add header to chomosome gene data
+#Author: Aude "bash woman" 
+#Last modified Fri Dec 9, 2016
+#for question, contact <aude.bouagnon@ucsf.edu>
+
+
+echo "Starting combining script"
+for file in "$@"
+do
+	
+   	echo "$file"
+	cat raw/header.txt "$file" > processed/$file
+	python gc_gene_plot.py processed/$file
+
+done
+echo "Done!!"
+
+
+
